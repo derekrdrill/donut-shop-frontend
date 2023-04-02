b18557204eecef2524e7a42d5351ddf49a05b75e
Derek Drill <derekrdrill@gmail.com>
2023-04-02 13:00:06 -0400

    🧪 setting up root app tests and moving to 'app' folder

---
+++ src/app/App.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+import App from './App';
+
+const renderer = new ShallowRenderer();
+
+describe('App unit tests', () => {
+  it('renders as expected', () => {
+    renderer.render(<App />);
+    const result = renderer.getRenderOutput();
+    expect(result).toMatchSnapshot();
+  });
+});
