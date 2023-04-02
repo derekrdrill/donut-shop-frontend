cff6bfd2d884d10a98b0cfbeccbd49d88c41c0dd
Derek Drill <derekrdrill@gmail.com>
2023-04-02 11:02:25 -0400

    🎉 First commit of retail website prototype foundation

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>Retail Website Prototype!</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    port: 5002,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader',
+        options: { limit: false },
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.jsx'],
+  },
+};
