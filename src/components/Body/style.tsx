0922787d8c724514808b213e025f7b3546a82e1a
Derek Drill <derekrdrill@gmail.com>
2023-04-08 23:32:11 -0400

    🔧 styled comp refactor

--- src/components/Body/style.tsx
+++ src/components/Body/style.tsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 import { Grid } from '@mui/material';
 
-export const BodyRoot = styled(Grid)<{ $isSmall?: boolean; $isLarge?: boolean }>(
-  ({ $isLarge }) => ({
-    position: 'relative',
-    // top: $isLarge ? 120 : 175,
-    top: 165,
-    zIndex: 0,
-  }),
-);
+export const BodyRoot = styled(Grid)({
+  position: 'relative',
+  top: 165,
+  zIndex: 0,
+});
+
