677fdfc5818854e6b40bd8400dc8d6203f4bed97
Derek Drill <derekrdrill@gmail.com>
2023-04-08 23:35:53 -0400

    👷‍♂️ removing unused import to fix deploy

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 
 import { GridProps, ThemeProvider } from '@mui/material';
 
-import { Breakpoints } from '../../hooks/useBreakpoint';
-
 import { BodyRoot } from './style';
 
 const Body = ({ children }: GridProps) => {
