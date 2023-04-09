5b3a5868da64faeb2db59b4ee3e5cb9b96e4a162
Derek Drill <derekrdrill@gmail.com>
2023-04-09 10:00:40 -0400

    🪲 setting body position to fixed when sidebar is open

--- src/app/style.tsx
+++ src/app/style.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle<{ $isSidebarOpen: boolean }>(({ $isSidebarOpen }) => [
   $isSidebarOpen && {
     body: {
+      position: 'fixed',
       overflowY: 'hidden',
     },
   },
