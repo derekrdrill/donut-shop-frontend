c1b9e9806e3f3e785867c93abdd17db9c65978ce
Derek Drill <derekrdrill@gmail.com>
2023-04-10 23:41:25 -0400

    🧪 unit test updates

--- src/pages/Menu/components/FullMenu/components/FullMenuBody.test.tsx
+++ src/pages/Menu/components/FullMenu/components/FullMenuBody.test.tsx
@@ -1,15 +1,21 @@
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => jest.fn(),
+}));
+
 import '@testing-library/jest-dom';
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
 import FullMenuBody from './FullMenuBody';
 
-import FULL_MENU_CATEGORIES from '../assets/data/FULL_MENU_CATEGORIES';
+import FULL_MENU from '../../../assets/data/FULL_MENU';
+import FULL_MENU_CATEGORIES from '../../../assets/data/FULL_MENU_CATEGORIES';
 
 describe('FullMenuBody unit tests', () => {
   it('renders as expected', () => {
     const result = renderer
-      .create(<FullMenuBody fullMenuCategories={FULL_MENU_CATEGORIES} />)
+      .create(<FullMenuBody fullMenuCategories={FULL_MENU_CATEGORIES} fullMenu={FULL_MENU} />)
       .toJSON();
     expect(result).toMatchSnapshot();
   });
