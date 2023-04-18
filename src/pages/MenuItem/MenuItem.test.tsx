145bd312a1c8ba742889ee16dedea991fee93904
Derek Drill <derekrdrill@gmail.com>
2023-04-18 10:06:16 -0400

    🧪 unit test updates

--- src/pages/MenuItem/MenuItem.test.tsx
+++ src/pages/MenuItem/MenuItem.test.tsx
@@ -9,8 +9,11 @@ jest.mock('react-router-dom', () => ({
 import '@testing-library/jest-dom';
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 
 import MenuItem from './MenuItem';
+
+import { MenuItemDetailText } from './style';
 
 describe('FullMenu unit tests', () => {
   it('renders as expected', () => {
@@ -30,5 +33,21 @@ describe('FullMenu unit tests', () => {
       )
       .toJSON();
     expect(result).toMatchSnapshot();
+  });
+
+  it('renders MenuItemDetailText correctly (isSubText)', async () => {
+    render(<MenuItemDetailText data-testid='isSubtext' $isSubtext />);
+
+    const menuItemDetailText = await screen.findAllByTestId('isSubtext');
+
+    expect(menuItemDetailText[0]).toHaveClass('sc-hLseeU hefLba');
+  });
+
+  it('renders MenuItemDetailText correctly (fontColor)', async () => {
+    render(<MenuItemDetailText data-testid='fontColor' $fontColor='red' />);
+
+    const menuItemDetailText = await screen.findAllByTestId('fontColor');
+
+    expect(menuItemDetailText[0]).toHaveClass('sc-hLseeU jQZkzZ');
   });
 });
