5068997e2f13108005c53cd0f88fccbb6c9f6a29
Derek Drill <derekrdrill@gmail.com>
2023-08-27 12:07:09 -0400

    🪲 fixing menu pg images

--- components/morningDough/Menu/components/FullMenu/components/FullMenuHead.tsx
+++ components/morningDough/Menu/components/FullMenu/components/FullMenuHead.tsx
@@ -8,11 +8,6 @@ import {
   FullMenuText,
 } from '../style';
 
-import icedCoffee from '../../../assets/images/coffeeAndTea/icedCoffee.png';
-import chocolateFrostingSprinklesDonut from '../../../assets/images/donuts/chocolateFrostingSprinklesDonut.png';
-import everythingBagel from '../../../assets/images/bread/everythingBagel.png';
-import hotLatte from '../../../assets/images/coffeeAndTea/hotLatte.png';
-
 const FullMenuHead = () => (
   <Grid container>
     <Grid item xs={12}>
@@ -20,10 +15,10 @@ const FullMenuHead = () => (
         <Grid item xs={12} sm={6} md={3} order={{ xs: 1, lg: 0 }}>
           <Grid container justifyContent={{ xs: 'center', lg: 'flex-start' }}>
             <Grid item xs={6} md={12} lg={6}>
-              <FullMenuHeadImage src={icedCoffee} />
+              <FullMenuHeadImage src='/icedCoffee.png' />
             </Grid>
             <Grid item xs={6} md={12} lg={6} display={{ xs: 'block', lg: 'none' }}>
-              <FullMenuHeadImage src={everythingBagel} />
+              <FullMenuHeadImage src='/everythingBagel.png' />
             </Grid>
           </Grid>
         </Grid>
@@ -34,19 +29,13 @@ const FullMenuHead = () => (
           <FullMenuText textAlign={{ xs: 'left', lg: 'center' }} variant='h3' $fontColor='#6a017f'>
             We'll be ready 😎
           </FullMenuText>
-          <FullMenuText
-            textAlign={{ xs: 'left', lg: 'center' }}
-            variant='h6'
-            $fontColor='#1c1c1c'
-            $isSubtext
-          >
+          <FullMenuText textAlign={{ xs: 'left', lg: 'center' }} variant='h6' $fontColor='#1c1c1c'>
             Special tasty treats just for you
           </FullMenuText>
           <FullMenuText
             textAlign={{ xs: 'left', lg: 'center' }}
             variant='body2'
             $fontColor='#5e5e5e'
-            $isSubtext
           >
             $1 croissants or FREE small coffee with the pruchase of an avacado toast. Price and
             participation may vary. Limited time offer. Terms apply.
@@ -55,10 +44,10 @@ const FullMenuHead = () => (
         <Grid item xs={12} sm={6} md={3} order={2}>
           <Grid container justifyContent={{ lg: 'center' }}>
             <Grid item xs={6} md={12} lg={6}>
-              <FullMenuHeadImage src={chocolateFrostingSprinklesDonut} />
+              <FullMenuHeadImage src='/chocolateFrostingSprinklesDonut.png' />
             </Grid>
             <Grid item xs={6} md={12} lg={6} display={{ xs: 'block', lg: 'none' }}>
-              <FullMenuHeadImage src={hotLatte} />
+              <FullMenuHeadImage src='/hotLatte.png' />
             </Grid>
           </Grid>
         </Grid>
